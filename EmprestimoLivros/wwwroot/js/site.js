67fa8db2c1447c0ff348373104e64ede452df069
luquesantosst <lucasgoncalves.st@gmail.com>
2023-07-10 22:39:02 -0300

    ajustes de layout, criação de filto

--- EmprestimoLivros/wwwroot/js/site.js
+++ EmprestimoLivros/wwwroot/js/site.js
@@ -2,3 +2,36 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+
+    $('#Emprestimos').DataTable({
+        language:
+        {
+            "decimal": "",
+            "emptyTable": "Sem dados disponíveis na tabela.",
+            "info": "Monstrando _START_ registro de _END_ em um _TOTAL_ entradas",
+            "infoEmpty": "Showing 0 to 0 of 0 entries",
+            "infoFiltered": "(filtered from _MAX_ total entries)",
+            "infoPostFix": "",
+            "thousands": ",",
+            "lengthMenu": "Mostrar _MENU_ entradas",
+            "loadingRecords": "Loading...",
+            "processing": "",
+            "search": "Procurar:",
+            "zeroRecords": "No matching records found",
+            "paginate": {
+                "first": "Primeiro",
+                "last": "Ultimo",
+                "next": "Próximo",
+                "previous": "Anterior"
+            },
+            "aria": {
+                "sortAscending": ": activate to sort column ascending",
+                "sortDescending": ": activate to sort column descending"
+
+            }
+        }
+    });
+
+
+})
